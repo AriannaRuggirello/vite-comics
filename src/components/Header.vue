77e0c117a0635125a8daab95a569fe52b647507b
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-31 15:30:25 +0200

    header layout

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<template>
+    <header>
+        <img src="" alt="logo">
+        <div>
+            <a href="#">link1</a>
+            <a href="#">link1</a>
+            <a href="#">link1</a>
+            <a href="#">link1</a>
+
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    background-color: greenyellow;
+    height: 90px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
