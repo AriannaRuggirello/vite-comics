19bad0ea3e10b371ee093e4ee33360f6653b7b85
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-06-01 17:23:25 +0200

    maincards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,93 @@
 
 <script>
+import MainCards from './MainCards.vue'
+
+
 export default {
     name: "AppMain",
+    components: {
+        MainCards
+    },
+    data() {
+        return {
+            slides: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
+                }
+            ]
+        }
+
+    }
+
 }
 
 
@@ -9,10 +95,14 @@ export default {
 
 <template>
     <main>
-        <div class="hero">
+        <div class="hero"></div>
+        <div class="container ">
+            <button>CURRENT SERIES</button>
+            <div class="main-cards">
+                <MainCards v-for="(slide, index) in slides" :key="index" :details="slide" />
 
-        </div>
-        <div class="container">
+            </div>
+
 
         </div>
 
@@ -22,21 +112,34 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/partials/variables.scss' as *;
 @use '../styles/partials/mixin.scss' as *;
-@use '../styles/general.scss' as *;
+
 
 main {
+    background-color: black;
 
     .hero {
         height: 300px;
         background-image: url("/jumbotron.jpg");
-        background-size: cover
+        background-size: cover;
     }
 
-    .container {
-        @include content-center;
+    button {
+        background-color: $primary-color ;
+        color: white;
+        border-style: none;
+        padding: 10px;
+        font-weight: bold;
+    }
 
-
+    .main-cards {
+        display: flex;
+        flex-wrap: wrap;
+        padding-top: 30px;
+        padding-bottom: 30px;
 
     }
+
+
+
 }
 </style>
---
+++ src/components/MainCards.vue
@@ -0,0 +1,39 @@
+
+<script>
+
+export default {
+    name: "MainCards",
+    props: {
+        details: Object,
+    }
+
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <img :src="details.thumb">
+        <h3>{{ details.series }}</h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+@use '../styles/partials/mixin.scss' as *;
+
+.card {
+    width: calc(100% / 6);
+    color: white;
+    text-align: center;
+
+
+    img {
+        width: 200px;
+        height: 200px;
+        object-fit: contain;
+    }
+
+
+}
+</style>
